21f353778d87da7cae6e03a1b6f953bce7c99b89
Matheus <matheusuky@gmail.com>
2022-07-23 00:13:01 -0300

    spinlock change
    add a pause instruction in order to not completely demolish the system bus in a lock

--- src/include/kernel/spinlock.h
+++ src/include/kernel/spinlock.h
@@ -1,8 +1,10 @@
 #ifndef _SPINLOCK_H_INCLUDE
 #define _SPINLOCK_H_INCLUDE
 
+// TODO rewrite this in a more arch independent way
+
 static inline void spinlock_acquire(int *lock){
-	while(!__sync_bool_compare_and_swap(lock, 0, 1));
+	while(!__sync_bool_compare_and_swap(lock, 0, 1)) asm("pause");
 }
 
 static inline void spinlock_release(int *lock){
