607b82ed6ceed066859d50265dc337d26a48bbeb
Matheus <matheusuky@gmail.com>
2022-07-24 18:09:25 -0300

    change pointers used in function calls
    change from void* to cls_t* as it makes more sense to be this way

--- src/arch/x86_64/cls.c
+++ src/arch/x86_64/cls.c
@@ -5,12 +5,12 @@
 cls_t bspcls;
 
 
-void arch_setcls(void* addr){
+void arch_setcls(cls_t* addr){
 	wrmsr(MSR_GSBASE, (uint64_t)addr);
 }
 
-void* arch_getcls(){
-	return (void*)rdmsr(MSR_GSBASE);
+cls_t* arch_getcls(){
+	return (cls_t*)rdmsr(MSR_GSBASE);
 }
 
 void bsp_setcls(){
--- src/arch/x86_64/include/arch/cls.h
+++ src/arch/x86_64/include/arch/cls.h
@@ -5,7 +5,7 @@
 #include <arch/ist.h>
 #include <arch/ist.h>
 #include <arch/regs.h>
-#include <arch/mmu.h>
+#include <kernel/vmm.h>
 
 // cpu level storage
 // this will be pointed to by GS and will contain per cpu info
@@ -14,11 +14,12 @@ typedef struct{
 	gdt_t gdt;
 	ist_t ist;
 	arch_regserror *laststate;
+	vmm_context *context;
 } cls_t;
 
 void bsp_setcls();
-void arch_setcls(void* addr);
-void* arch_getcls();
+void arch_setcls(cls_t* addr);
+cls_t* arch_getcls();
 
 static cls_t __seg_gs *cls = 0;
 
