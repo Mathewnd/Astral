417c46c0405bef25ab8ef6b7cf227c7affea7a34
Matheus <matheusuky@gmail.com>
2022-09-09 18:14:14 -0300

    spinlock: try and acquire func

--- src/arch/x86_64/include/arch/spinlock.h
+++ src/arch/x86_64/include/arch/spinlock.h
@@ -1,8 +1,14 @@
 #ifndef _SPINLOCK_H_INCLUDE
 #define _SPINLOCK_H_INCLUDE
 
+#include <stdbool.h>
+
 static inline void spinlock_acquire(int *lock){
 	while(!__sync_bool_compare_and_swap(lock, 0, 1)) asm("pause");
+}
+
+static inline bool spinlock_trytoacquire(int *lock){
+	return __sync_bool_compare_and_swap(lock, 0, 1);
 }
 
 static inline void spinlock_release(int *lock){
