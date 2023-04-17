f66140c5783144a3d84839433b6838305a69a3db
Matheus <matheusuky@gmail.com>
2023-04-16 23:53:22 -0300

    util.h: fix round_up

--- kernel-src/include/util.h
+++ kernel-src/include/util.h
@@ -1,7 +1,7 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
-#define ROUND_UP(v, n) (n - (v % n) + v)
-#define ROUND_DOWN(v, n) (v - (v % n))
+#define ROUND_DOWN(v, n) ((v) - ((v) % (n)))
+#define ROUND_UP(v, n) ROUND_DOWN((v) + (n) - 1, n)
 
 #endif
