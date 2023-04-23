b57cb385ce88a7bf9b2594ca2d2033bf8fa1d542
Matheus <matheusuky@gmail.com>
2023-04-22 21:26:44 -0300

    time: timespec_add()

--- kernel-src/include/time.h
+++ kernel-src/include/time.h
@@ -8,4 +8,11 @@ typedef struct {
 	time_t ns;
 } timespec_t;
 
+static inline timespec_t timespec_add(timespec_t a, timespec_t b) {
+	timespec_t ts;
+	ts.ns = (a.ns + b.ns) % 1000000000;
+	ts.s = a.s + b.s + (a.ns + b.ns) / 1000000000;
+	return ts;
+}
+
 #endif
