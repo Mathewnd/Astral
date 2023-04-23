ee63940c68f23c5239664a0c0ebb86f204b74140
Matheus <matheusuky@gmail.com>
2023-04-23 15:06:34 -0300

    logging: make failed asserts panic

--- kernel-src/include/logging.h
+++ kernel-src/include/logging.h
@@ -2,16 +2,15 @@
 #define _LOGGING_H
 
 #include <printf.h>
+#include <panic.h>
 
 void _putchar(char c);
 void logging_sethook(void (*fun)(char));
 
-// TODO panic
-
 #define __assert(x) \
 	if (!(x)){ \
 		printf("\e[91m%s:%s:%d: %s failed\n\e[0m", __FILE__, __func__, __LINE__, #x); \
-		for(;;); \
+		_panic(NULL, NULL); \
 	};
 
 
