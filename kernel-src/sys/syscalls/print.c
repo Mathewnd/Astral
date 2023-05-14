220416e16fafe4b0591fcbb220310537bec8b774
Matheus <matheusuky@gmail.com>
2023-05-13 22:29:54 -0300

    kernel: default print to e9

--- kernel-src/sys/syscalls/print.c
+++ kernel-src/sys/syscalls/print.c
@@ -1,11 +1,14 @@
 #include <kernel/syscalls.h>
 #include <printf.h>
+#include <arch/e9.h>
 
 syscallret_t syscall_print(context_t *context, char *message) {
 	syscallret_t ret = {
 		.ret = 0,
 		.errno = 0
 	};
-	printf("%s\n", message);
+	//printf("%s\n", message);
+	arch_e9_puts(message);
+	arch_e9_putc('\n');
 	return ret;
 }
