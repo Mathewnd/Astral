47910a616fe961862b3d5025c11ca8bb72b2bbe9
Matheus <matheusuky@gmail.com>
2024-04-14 19:09:58 -0300

    syscalls: gettid

---
+++ kernel-src/sys/syscalls/gettid.c
@@ -0,0 +1,10 @@
+#include <kernel/syscalls.h>
+#include <kernel/scheduler.h>
+
+syscallret_t syscall_gettid(context_t *) {
+	syscallret_t ret = {
+		.errno = 0,
+		.ret = _cpu()->thread->tid
+	};
+	return ret;
+}
