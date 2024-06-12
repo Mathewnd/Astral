98c5963893332f6df1c015364fd6a7fcde687524
Matheus <matheusuky@gmail.com>
2024-06-12 19:09:03 -0300

    accept: return address

--- kernel-src/sys/syscalls/accept.c
+++ kernel-src/sys/syscalls/accept.c
@@ -3,11 +3,17 @@
 #include <kernel/sock.h>
 #include <errno.h>
 
-// TODO handle abisockaddr
-syscallret_t syscall_accept(context_t *, int oldfd, abisockaddr_t *abisockaddr, int *addrlen, int acceptflags) {
+syscallret_t syscall_accept(context_t *, int oldfd, abisockaddr_t *abisockaddr, int *uaddrlen, int acceptflags) {
 	syscallret_t ret = {
 		.ret = -1
 	};
+
+	int addrlen = 0;
+	if (abisockaddr) {
+		ret.errno = usercopy_fromuser(&addrlen, uaddrlen, sizeof(int));
+		if (ret.errno)
+			return ret;
+	}
 
 	file_t *oldfile = fd_get(oldfd);
 	if (oldfile == NULL) {
@@ -57,6 +63,16 @@ syscallret_t syscall_accept(context_t *, int oldfd, abisockaddr_t *abisockaddr, 
 	newfile->flags = FILE_READ | FILE_WRITE | (acceptflags & (O_NONBLOCK));
 	newfile->offset = 0;
 	newfile->mode = 0777;
+
+	abisockaddr_t tmpabiaddr;
+	__assert(sock_addrtoabiaddr(client->type, &addr, &tmpabiaddr) == 0);
+	addrlen = min(addrlen, sizeof(abisockaddr_t));
+
+	if (abisockaddr != NULL && (usercopy_touser(abisockaddr, &tmpabiaddr, addrlen) || usercopy_touser(uaddrlen, &addrlen, sizeof(addrlen)))) {
+		ret.errno = EFAULT;
+		fd_close(newfd);
+		goto cleanup;
+	}
 
 	ret.ret = newfd;
 
