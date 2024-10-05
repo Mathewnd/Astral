8315d5642e878206463faf2eb2aed8dc0ce31281
Matheus <matheusuky@gmail.com>
2024-10-05 11:31:35 -0300

    spinlock: SPINLOCK_DEFINE

--- kernel-src/include/spinlock.h
+++ kernel-src/include/spinlock.h
@@ -6,8 +6,9 @@
 
 typedef int spinlock_t;
 
-#define SPINLOCK_INIT(x) x = 0;
 #define SPINLOCK_INIT_VALUE 0
+#define SPINLOCK_INIT(x) x = SPINLOCK_INIT_VALUE
+#define SPINLOCK_DEFINE(x) spinlock_t x = SPINLOCK_INIT_VALUE
 
 static inline void spinlock_acquire(spinlock_t *lock){
 	while(!__sync_bool_compare_and_swap(lock, 0, 1)) asm("pause");
