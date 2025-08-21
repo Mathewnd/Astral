1787658989bcbdbf7b2d7f1a15577a18e2fc1a3b
Matheus <matheusuky@gmail.com>
2025-08-21 20:53:05 -0300

    nice: add auth handling

--- kernel-src/sys/syscalls/nice.c
+++ kernel-src/sys/syscalls/nice.c
@@ -1,15 +1,12 @@
 #include <kernel/syscalls.h>
 #include <kernel/thread.h>
+#include <kernel/auth.h>
 #include <arch/cpu.h>
 
 syscallret_t syscall_nice(context_t *, int nice) {
 	syscallret_t ret;
 
-
-	if (nice < 0) {
-		
-	}
-
+	ret.errno = nice < 0 ? auth_process_check(&current_thread()->proc->cred, AUTH_ACTIONS_PROCESS_NICE, NULL) : 0;
 	current_thread()->proc->nice = max(min(current_thread()->proc->nice + nice, SCHED_NICE_MAX), SCHED_NICE_MIN);
 
 	ret.ret = current_thread()->proc->nice;
