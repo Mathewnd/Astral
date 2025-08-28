32f02edcc848003bb557429ebeef1d1563c2542d
Matheus <matheusuky@gmail.com>
2025-08-27 21:52:13 -0300

    panic: halt other cpus

--- kernel-src/lib/panic.c
+++ kernel-src/lib/panic.c
@@ -1,9 +1,11 @@
 #include <panic.h>
 #include <arch/cpu.h>
+#include <arch/smp.h>
 #include <printf.h>
 #include <kernel/interrupt.h>
 
 __attribute__((noreturn)) void _panic(char *msg, context_t *ctx) {
+	interrupt_set(false);
 	printf("cpu%lu: Oops.\n", current_cpu_id());
 
 	if (msg)
@@ -12,6 +14,6 @@ __attribute__((noreturn)) void _panic(char *msg, context_t *ctx) {
 	if (ctx)
 		PRINT_CTX(ctx);
 
-	interrupt_set(false);
+	arch_smp_haltallothers();
 	for (;;);
 }
