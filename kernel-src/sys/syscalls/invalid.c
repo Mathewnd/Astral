e73ebf225242ad59c51b3dcf1500807217c370cb
Matheus <matheusuky@gmail.com>
2025-11-02 21:38:44 -0300

    syscalls/invalid: raise SIGSYS and print message on an invalid system call

--- kernel-src/sys/syscalls/invalid.c
+++ kernel-src/sys/syscalls/invalid.c
@@ -1,4 +1,5 @@
 #include <kernel/syscalls.h>
+#include <arch/cpu.h>
 #include <errno.h>
 
 syscallret_t syscall_invalid() {
@@ -6,5 +7,10 @@ syscallret_t syscall_invalid() {
 		.errno = ENOSYS,
 		.ret = -1
 	};
+
+
+	printf("Detected invalid system call, raising SIGSYS and returning ENOSYS\n");
+	signal_signalthread(current_thread(), SIGSYS, true);
+
 	return ret;
 }
