9bc0e2580181fb640a0f3cf972af53ebda7be649
Matheus <matheusuky@gmail.com>
2026-04-27 00:05:18 -0300

    bitmap: add BITMAP_DEFINE_STATIC

--- kernel-src/include/bitmap.h
+++ kernel-src/include/bitmap.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <util.h>
 
 typedef struct {
 	long *data;
@@ -15,5 +16,12 @@ void bitmap_set(bitmap_t *bitmap, long idx, int v);
 void bitmap_set_range(bitmap_t *bitmap, long start, long count, int v);
 bool bitmap_get(bitmap_t *bitmap, long idx);
 long bitmap_find_first_set(bitmap_t *bitmap);
+
+#define BITMAP_DEFINE_STATIC(name, s) \
+	static long entries_bm_##name[ROUND_UP(s, sizeof(long)) / sizeof(long)]; \
+	static bitmap_t name = { \
+		.size = s, \
+		.data = entries_bm_##name \
+	}
 
 #endif
