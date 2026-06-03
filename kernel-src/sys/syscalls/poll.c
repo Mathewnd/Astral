2f27f20b1abf73fa216c4d9ca2530a04373c8e39
Matheus <matheusuky@gmail.com>
2026-06-03 20:07:50 -0300

    poll: fix ppoll signal masking

--- kernel-src/sys/syscalls/poll.c
+++ kernel-src/sys/syscalls/poll.c
@@ -134,10 +134,15 @@ syscallret_t syscall_ppoll(context_t *context, pollfd_t *fds, size_t nfds, times
 			return ret;
 
 		signal_changemask(current_thread(), SIG_SETMASK, &sigset, &savedset);
-		signal_returnmask(current_thread(), &savedset);
 	}
 
 	syscallret_t ret = syscall_poll(context, fds, nfds, utimeout ? (timeout.s * 1000 + timeout.ns / 1000000) : -1);
+	if (usigset) {
+		if (ret.errno == EINTR)
+			signal_returnmask(current_thread(), &savedset);
+		else
+			signal_changemask(current_thread(), SIG_SETMASK, &savedset, NULL);
+	}
 
 	return ret;
 }
