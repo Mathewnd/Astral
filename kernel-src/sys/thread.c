57879f48dae7625655680e775713564766922380
Samuel Stuart <sasdallas765@gmail.com>
2026-07-07 17:09:55 -0300

    thread: fix release of proc refcounts on multithreaded applications

--- kernel-src/sys/thread.c
+++ kernel-src/sys/thread.c
@@ -96,8 +96,9 @@ __attribute__((noreturn)) void sched_threadexit() {
 
 			proc_exit();
 			mm_destroy_context(oldctx);
-			PROC_RELEASE(proc);
 		}
+
+		PROC_RELEASE(proc);
 	}
 
 	interrupt_set(false);
